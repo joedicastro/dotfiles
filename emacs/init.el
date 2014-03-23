837c38084bf924d85efa52b7b1a10ef8a8d64668
joe di castro <joe@joedicastro.com>
2014-03-23 19:52:17 +0100

    vim: configure neomru cache directory

--- emacs/init.el
+++ emacs/init.el
@@ -15,3 +15,15 @@
 
 ; Load all literate org-mode files in this directory (any org-mode files residing there)
 (mapc #'org-babel-load-file (directory-files dotfiles-dir t "readme.org$"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("b52210647e2d2929cdc86e4ad801499739f4a6624079adacc45f6dc3df82575d" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
