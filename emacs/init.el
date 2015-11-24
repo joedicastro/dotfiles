0173e71a7839d536899fe5724e27f4d873a49ac5
joe di castro <joe@joedicastro.com>
2015-11-24 12:08:29 +0100

    emacs: remove twittering mode
    I have twitter in my smartphone/tablet, I don't use it for work and
    don't need it here. It helps also to avoid distractions on work.

--- emacs/init.el
+++ emacs/init.el
@@ -1190,8 +1190,7 @@
       prog-mode
       term-mode
       conf-mode
-      web-mode
-      twittering-edit-mode)
+      web-mode)
     "List of modes that should start up in Evil state."
     :type '(symbol))
 
@@ -1961,8 +1960,8 @@
     Browse       Search             Social               Post       │ Internet │
 ╭───────────────────────────────────────────────────────────────────┴──────────╯
   [_w_] eww      [_g_] google          [_f_] elfeed            [_i_] imgur
-  [_u_] url      [_m_] google maps     [_t_] twitter
-   ^ ^           [_s_] surfraw         [_x_] stack overflow
+  [_u_] url      [_m_] google maps     [_x_] stack overflow
+   ^ ^           [_s_] surfraw
    ^ ^           [_d_] wordnik
 --------------------------------------------------------------------------------
       "
@@ -1972,7 +1971,6 @@
       ("m" google-maps)
       ("d" define-word-at-point)
       ("s" helm-surfraw)
-      ("t" twit)
       ("w" eww)
       ("u" browse-url-at-point)
       ("x" sx-tab-newest))
@@ -3534,128 +3532,9 @@
      (add-to-list 'regexp-search-ring (ivy--regex ivy-text)))
   (ivy-mode t)
   (use-package counsel
+    :init
+    (bind-key "<menu>" 'counsel-M-x)
     :ensure t))
-
-;; twittering-mode
-
-;; [[https://github.com/hayamiz/twittering-mode][Twittering-mode]] enables you to twit on Emacsen.
-
-;; [[./img/twittering_mode.png]]
-
-(use-package twittering-mode
-  :ensure t
-  :commands twit
-  :init
-  (add-hook 'twittering-edit-mode-hook (lambda () (flyspell-mode)))
-  (add-hook 'twittering-mode-hook (lambda () (variable-pitch-mode)))
-  :config
-  (setq twittering-use-master-password t
-        twittering-icon-mode t
-        twittering-use-icon-storage t
-        twittering-icon-storage-file (concat joe-emacs-temporal-directory "twittering-mode-icons.gz")
-        twittering-convert-fix-size 52
-        twittering-initial-timeline-spec-string '(":home")
-        twittering-edit-skeleton 'inherit-any
-        twittering-display-remaining t
-        twittering-timeline-header  "─────────────────────────────────────────────────────────────────────────────\n"
-        twittering-timeline-footer  "------------------------------------------------------------------------------------------------------------------------------------------\n"
-        twittering-status-format
-        "%i  %S, %RT{%FACE[bold]{%S}} %@  %FACE[shadow]{%p%f%L%r}\n%FOLD[             ]{%T}\n")
-
-    ;; set the new bindings
-    (bind-keys :map twittering-mode-map
-               ("q" . twittering-kill-buffer)
-               ("Q" . twittering-edit-mode)
-               ("j" . twittering-goto-next-status)
-               ("k" . twittering-goto-previous-status)
-               ("h" . twittering-switch-to-next-timeline)
-               ("l" . twittering-switch-to-previous-timeline)
-               ("g" . beginning-of-buffer)
-               ("G" . end-of-buffer)
-               ("t" . twittering-update-status-interactive)
-               ("X" . twittering-delete-status)
-               ("RET" . twittering-reply-to-user)
-               ("r" . twittering-native-retweet)
-               ("R" . twittering-organic-retweet)
-               ("d" . twittering-direct-message)
-               ("u" . twittering-current-timeline)
-               ("b" . twittering-favorite)
-               ("B" . twittering-unfavorite)
-               ("f" . twittering-follow)
-               ("F" . twittering-unfollow)
-               ("i" . twittering-view-user-page)
-               ("/" . twittering-search)
-               ("." . twittering-visit-timeline)
-               ("@" . twittering-other-user-timeline)
-               ("T" . twittering-toggle-or-retrieve-replied-statuses)
-               ("o" . twittering-click)
-               ("TAB" . twittering-goto-next-thing)
-               ("<backtab>" . twittering-goto-previous-thing)
-               ("n" . twittering-goto-next-status-of-user)
-               ("p" . twittering-goto-previous-status-of-user)
-               ("SPC" . twittering-scroll-up)
-               ("S-SPC" . twittering-scroll-down)
-               ("y" . twittering-push-uri-onto-kill-ring)
-               ("Y" . twittering-push-tweet-onto-kill-ring)
-               ("a" . twittering-toggle-activate-buffer))
-    (when (package-installed-p 'hydra)
-      (bind-keys :map twittering-mode-map
-               ("\\" . hydra-twittering/body))
-      (defhydra hydra-twittering (:color blue :hint nil)
-        "
-                                                                    ╭────────────┐
-     tweets                user                        timeline     │ twittering │
-  ╭─────────────────────────────────────────────────────────────────┴────────────╯
-    _k_  [_t_] post tweet      _p_  [_f_] follow                  ^_g_^      [_u_] update
-    ^↑^  [_X_] delete tweet    ^↑^  [_F_] unfollow              ^_S-SPC_^    [_._] new
-    ^ ^  [_r_] retweet         ^ ^  [_d_] direct message          ^^↑^^      [^@^] current user
-    ^↓^  [_R_] retweet & edit  ^↓^  [_i_] profile (browser)   _h_ ←   → _l_  [_a_] toggle
-    _j_  [_b_] favorite        _n_   ^ ^                          ^^↓^^
-    ^ ^  [_B_] unfavorite      ^ ^   ^ ^                         ^_SPC_^
-    ^ ^  [_RET_] reply         ^ ^   ^ ^                          ^_G_^
-    ^ ^  [_t_] show thread
-    ^ ^  [_y_] yank url          items                     do
-    ^ ^  [_Y_] yank tweet     ╭───────────────────────────────────────────────────────
-    ^ ^  [_e_] edit mode        _<backtab>_ ← _o_pen → _<tab>_    [_q_] exit
-    ^ ^   ^ ^                   ^         ^   ^ ^      ^     ^    [_/_] search
-  --------------------------------------------------------------------------------
-        "
-       ("\\" hydra-master/body "back")
-       ("<ESC>" nil "quit")
-       ("q"          twittering-kill-buffer)
-       ("e"          twittering-edit-mode)
-       ("j"          twittering-goto-next-status :color red)
-       ("k"          twittering-goto-previous-status :color red)
-       ("h"          twittering-switch-to-next-timeline :color red)
-       ("l"          twittering-switch-to-previous-timeline :color red)
-       ("g"          beginning-of-buffer)
-       ("G"          end-of-buffer)
-       ("t"          twittering-update-status-interactive)
-       ("X"          twittering-delete-status)
-       ("RET"        twittering-reply-to-user)
-       ("r"          twittering-native-retweet)
-       ("R"          twittering-organic-retweet)
-       ("d"          twittering-direct-message)
-       ("u"          twittering-current-timeline)
-       ("B"          twittering-favorite)
-       ("b"          twittering-unfavorite)
-       ("f"          twittering-follow)
-       ("F"          twittering-unfollow)
-       ("i"          twittering-view-user-page)
-       ("/"          twittering-search)
-       ("."          twittering-visit-timeline)
-       ("@"          twittering-other-user-timeline)
-       ("T"          twittering-toggle-or-retrieve-replied-statuses)
-       ("o"          twittering-click)
-       ("<tab>"      twittering-goto-next-thing :color red)
-       ("<backtab>"  twittering-goto-previous-thing :color red)
-       ("n"          twittering-goto-next-status-of-user :color red)
-       ("p"          twittering-goto-previous-status-of-user :color red)
-       ("SPC"        twittering-scroll-up :color red)
-       ("S-SPC"      twittering-scroll-down :color red)
-       ("y"          twittering-push-uri-onto-kill-ring)
-       ("Y"          twittering-push-tweet-onto-kill-ring)
-       ("a"          twittering-toggle-activate-buffer))))
 
 ;; ujelly-theme
 
